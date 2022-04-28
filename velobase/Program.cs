0cd6f0e389bdf4f8c38119553eb6af7bb945f988
Alexander Romanenko <alex@alex-tech-adventures.com>
2022-04-28 11:26:14 +0300

    Practice writing 16 bit integer to disk.

--- velobase/Program.cs
+++ velobase/Program.cs
@@ -1,3 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+const String FILE_PATH = "C:\\devel\\velobase\\test.dat";
+
+WriteSampleData();
+using (FileStream stream = new FileStream(FILE_PATH, FileMode.Open))
+{
+    byte[] data = new byte[2];
+
+    stream.Read(data, 0, 2);
+
+    ushort val1 = BitConverter.ToUInt16(data, 0);
+    Console.WriteLine(val1);
+}
+
+void WriteSampleData()
+{
+    const ushort data = 5;
+
+    using (BinaryWriter writer = new BinaryWriter(new FileStream(FILE_PATH, FileMode.Create)))
+    {
+        writer.Write(data);
+
+        writer.Close();
+    }
+}
