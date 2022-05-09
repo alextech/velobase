5dbeb236ab8b8666e4fea4bfdf9dac3ff3dd9e50
Alexander Romanenko <alex@alex-tech-adventures.com>
2022-05-09 20:40:14 +0300

    Adding CSV sample sales records.

--- velobase/Program.cs
+++ velobase/Program.cs
@@ -1,16 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Collections;
-using System.Text;
+using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Storage;
 using Storage.Schema;
-using String = System.String;
+using velobase.SampleSales;
+using Char = Storage.Schema.Char;
 
-const String FILE_PATH = "C:\\devel\\velobase\\test.dat";
+const string FILE_PATH = "C:/devel/velobase/test.dat";
+const string SALES_RECORDS_DB_PATH = "C:/devel/velobase/sales_records.dat";
+const string SALES_RECORDS_CSV_PATH = "c:/devel/velobase/1000_Sales_Records.csv";
 
-WriteSampleData();
-Reader reader = new Reader(FILE_PATH);
+// WriteSampleData();
+
+// WriteSalesRecordsDb();
+
+
+Reader reader = new Reader(SALES_RECORDS_DB_PATH);
 IEnumerable<IEnumerable<object>> rows = reader.FetchAllRows();
+List<IEnumerable<object>> rowObjects = rows.ToList().ToList();
+
+foreach (var rowObject in rowObjects)
+{
+    Console.WriteLine(rowObject.ToList()[0]);
+}
+
+return;
 
 // reader.FetchByPK(5);
 // reader.FetchByIndexId("index_name", 5);
@@ -22,7 +38,7 @@ void WriteSampleData()
     Table table = new Table();
     table.AddField(new Integer());
     table.AddField(new Integer());
-    table.AddField(new Storage.Schema.Char());
+    table.AddField(new Char());
     table.AddField(new Integer());
 
     Writer writer = new Writer(table, FILE_PATH);
@@ -53,7 +69,76 @@ void WriteSampleData()
     writer.CommitRow();
 
 
-
-    
     // INSERT INTO tbl_name('col1', 'col2') VALUES('val1', 'val2');
+}
+
+void WriteSalesRecordsDb()
+{
+    Table table = new Table();
+    table.AddField(new Char()); // region
+    table.AddField(new Char()); // country
+    table.AddField(new Char()); // item_type
+    table.AddField(new Char()); // sales_channel
+    table.AddField(new Char()); // order_priority
+    table.AddField(new Integer()); // order_date
+    table.AddField(new Integer()); // order_id
+    table.AddField(new Integer()); // ship_date
+    table.AddField(new Integer()); // units_sold
+    table.AddField(new Integer()); // unit_price
+    table.AddField(new Integer()); // unit_cost
+    table.AddField(new Integer()); // total_revenue
+    table.AddField(new Integer()); // total_cost
+    table.AddField(new Integer()); // total_profit
+
+    Writer writer = new Writer(table, SALES_RECORDS_DB_PATH);
+    writer.WriteTableSchema();
+    
+    WriteSalesRecordsData(table);
+}
+
+void WriteSalesRecordsData(Table table)
+{
+    IEnumerable<SaleRecord> salesRecords = ReadSalesRecordsCsv();
+
+    Writer writer = new Writer(table, SALES_RECORDS_DB_PATH);
+    foreach (SaleRecord salesRecord in salesRecords)
+    {
+        writer.BeginWritingRow();
+        
+        writer.WriteValue(salesRecord.Region);
+        writer.WriteValue(salesRecord.Country);
+        writer.WriteValue(salesRecord.ItemType);
+        writer.WriteValue(salesRecord.SalesChannel);
+        writer.WriteValue(salesRecord.OrderPriority);
+        writer.WriteValue((int)(salesRecord.OrderDate.ToUnixTimeSeconds() % int.MaxValue));
+        writer.WriteValue(salesRecord.OrderID);
+        writer.WriteValue((int)(salesRecord.ShipDate.ToUnixTimeSeconds() % int.MaxValue));
+        writer.WriteValue(salesRecord.UnitsSold);
+        writer.WriteValue((int)salesRecord.UnitPrice);
+        writer.WriteValue((int)salesRecord.UnitCost);
+        writer.WriteValue((int)salesRecord.TotalRevenue);
+        writer.WriteValue((int)salesRecord.TotalCost);
+        writer.WriteValue((int)salesRecord.TotalProfit);
+        
+        writer.CommitRow();
+    }
+
+}
+
+IEnumerable<SaleRecord> ReadSalesRecordsCsv()
+{
+    CsvConfiguration config = new CsvConfiguration(CultureInfo.InvariantCulture)
+    {
+        PrepareHeaderForMatch = args => args.Header.Replace(" ", ""),
+    };
+
+    using (StreamReader reader = new StreamReader(SALES_RECORDS_CSV_PATH))
+    using (CsvReader csv = new CsvReader(reader, config))
+    {
+        IEnumerable<SaleRecord> saleRecords = csv.GetRecords<SaleRecord>();
+
+        IEnumerable<SaleRecord> readSaleRecords = saleRecords.ToList();
+
+        return readSaleRecords;
+    }
 }
---
+++ velobase/SampleSales/SaleRecord.cs
@@ -0,0 +1,19 @@
+﻿namespace velobase.SampleSales;
+
+public class SaleRecord
+{
+    public string Region { get; set; }
+    public string Country { get; set; }
+    public string ItemType { get; set; }
+    public string SalesChannel { get; set; }
+    public string OrderPriority { get; set; }
+    public DateTimeOffset OrderDate { get; set; }
+    public int OrderID { get; set; }
+    public DateTimeOffset ShipDate { get; set; }
+    public int UnitsSold { get; set; }
+    public decimal UnitPrice { get; set; }
+    public decimal UnitCost { get; set; }
+    public decimal TotalRevenue { get; set; }
+    public decimal TotalCost { get; set; }
+    public decimal TotalProfit { get; set; }
+}
